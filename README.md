e0c206150939ab5811531159563bb92b57da2e84
Luca <nahuquilmes2000@gmail.com>
2023-07-23 16:00:44 -0300

    Bianluca finalizado

---
+++ README.md
@@ -0,0 +1,81 @@
+# Segundo Parcial - PPS
+# 🍜🍣🍙 Grupo Bianluca 🍙🍣🍜
+
+<img width="200px" text-align="center" src="src/assets/icon/icon.png">
+
+## Integrantes:
+- <a href="https://github.com/LucaAg">Luca Nahuel Agnoli</a>
+- <a href="https://github.com/biancacasetta">Bianca Casetta</a>
+- <a href="https://github.com/tkz00">Theo Katz</a>
+
+## Capturas de pantalla
+
+#### Splash Screen
+
+<img width="200px" src="src/assets/readme/splash.jpg">
+
+#### Login
+<img width="200px" src="src/assets/readme/login.jpg">
+
+#### Registro
+<table>
+  <tr>
+    <td><img width="200px" src="src/assets/readme/registro-cliente-pagina1.jpg"></td>
+    <td><img width="200px" src="src/assets/readme/registro-cliente-pagina2.jpg"></td>
+    <td><img width="200px" src="src/assets/readme/registro-cliente-pagina3.jpg"></td>
+    <td><img width="200px" src="src/assets/readme/registro-anonimo.jpg"></td>
+  </tr>
+</table>
+
+### Vistas según el perfil
+
+| Dueño/Supervisor |
+|:----------------:|
+| <img width="200px" src="src/assets/readme/dueno-supervisor.jpg">|
+
+| Metre         |
+|:-------------:|
+| <img width="200px" src="src/assets/readme/metre.jpg">|
+
+#### Mozo
+<img width="200px" src="src/assets/readme/home_mozo.jpg">
+
+#### Cocinero
+<img width="200px" src="src/assets/readme/home_cocinero.jpg">
+
+#### Cliente
+
+<table>
+  <tr>
+    <td><img width="200px" src="src/assets/readme/home%20cliente.jpg"></td>
+    <td><img width="200px" src="src/assets/readme/cliente%20en%20espera.jpg"></td>
+    <td><img width="200px" src="src/assets/readme/cliente%20sentado%20sin%20mesa%20escaneada.jpg"></td>
+    <td><img width="200px" src="src/assets/readme/mesa.jpg"></td>
+  </tr>
+  <tr>
+    <td><img width="200px" src="src/assets/readme/mesa-menu.jpg"></td>
+    <td><img width="200px" src="src/assets/readme/mesa-detalle-pedido.jpg"></td>
+    <td><img width="200px" src="src/assets/readme/mesa-pedido-realizado.jpg"></td>
+    <td><img width="200px" src="src/assets/readme/mesa-pedido-entregado.jpg"></td>
+  </tr>
+  <tr>
+    <td><img width="200px" src="src/assets/readme/mesa-pedido-recibido.jpg"></td>
+    <td><img width="200px" src="src/assets/readme/mesa-pedido-pago.jpg"></td>
+    <td><img width="200px" src="src/assets/readme/chat.jpg"></td>
+    <td><img width="200px" src="src/assets/readme/encuesta.jpg"></td>
+  </tr>
+</table>
+
+## Tareas por semana
+
+### Semana 1
+
+<img src="src/assets/readme/tareas_semana1.PNG">
+
+### Semana 2
+
+<img src="src/assets/readme/tareas_semana2.PNG">
+
+### Semana 3
+
+<img src="src/assets/readme/tareas_semana3.PNG">
---
+++ src/app/paginas/login/login.page.ts
@@ -0,0 +1,135 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { AudioService } from 'src/app/servicios/audio.service';
+import { AuthService } from 'src/app/servicios/auth.service';
+import { FirebaseService } from 'src/app/servicios/firebase.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  //@ts-ignore
+  formLogin: FormGroup;
+  emailPattern: any = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  spinner: boolean = false;
+  clientesPendientes: any[] = [];
+  clientesRechazados: any[] = [];
+  popup: boolean = false;
+  mensajePopup: string = "";
+  isAudioActive = false;
+
+  constructor(private formBuilder: FormBuilder, private auth: AuthService, private firestore: FirebaseService, private audioService: AudioService) {
+    this.formLogin = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.pattern(this.emailPattern)]],
+      password: ['', [Validators.required, Validators.pattern(".{6,}")]]
+    });
+  }
+
+  ngOnInit() {
+    this.isAudioActive = this.audioService.isActive;
+  }
+
+  ngAfterViewInit() {
+    this.firestore.obtenerColeccion("clientes-pendientes").subscribe((data) => {
+      this.clientesPendientes = data;
+    });
+    this.firestore.obtenerColeccion("clientes-rechazados").subscribe((data) => {
+      this.clientesRechazados = data;
+    });
+  }
+
+  verificarEmail(email: string) {
+    let mensaje = "";
+
+    this.clientesPendientes.forEach((cliente) => {
+      if (cliente.email == email) {
+        mensaje = "Pendiente";
+      }
+    });
+
+    this.clientesRechazados.forEach((cliente) => {
+      if (cliente.email == email) {
+        mensaje = "Rechazado";
+      }
+    });
+
+    return mensaje;
+  }
+
+  async login() {
+    this.spinner = true;
+
+    if (this.formLogin.valid) {
+      setTimeout(async () => {
+
+        let estado = this.verificarEmail(this.formLogin.value.email);
+        switch (estado) {
+          case "":
+            await this.auth.iniciarSesion(this.formLogin.value.email, this.formLogin.value.password).then(() => {
+              console.log("¡Login exitoso!");
+              this.formLogin.reset();
+            })
+              .catch((error) => {
+                this.mensajePopup = this.auth.crearMensaje(error.code);
+                this.popup = true;
+              });
+            break;
+          case "Pendiente":
+            this.mensajePopup = "Aún no se procesó tu registro.";
+            this.popup = true;
+            break;
+          case "Rechazado":
+            this.mensajePopup = "Se rechazó tu registro y no podés iniciar sesión.";
+            this.popup = true;
+            break;
+        }
+
+        this.spinner = false;
+      }, 2000);
+    }
+    else {
+      setTimeout(() => {
+        this.spinner = false;
+        this.mensajePopup = "Faltan completar campos";
+        this.popup = true;
+      }, 2000);
+    }
+  }
+
+  insertarAccesosRapidos(perfil: string) {
+    switch (perfil) {
+      case "dueño":
+        this.formLogin.get('email').setValue("duenio@duenio.com");
+        this.formLogin.get('password').setValue("duenio");
+        break;
+      case "metre":
+        this.formLogin.get('email').setValue("metre@metre.com");
+        this.formLogin.get('password').setValue("metre1");
+        break;
+      case "mozo":
+        this.formLogin.get('email').setValue("mozo1@mozos.com");
+        this.formLogin.get('password').setValue("mozos1");
+        break;
+      case "cocinero":
+        this.formLogin.get('email').setValue("cocinero@cocinero.com");
+        this.formLogin.get('password').setValue("cocinero");
+        break;
+      case "bartender":
+        this.formLogin.get('email').setValue("bartender@bartender.com");
+        this.formLogin.get('password').setValue("bartender");
+        break;
+      case "cliente":
+        this.formLogin.get('email').setValue("nahuquilmes2000@gmail.com");
+        this.formLogin.get('password').setValue("quilmes");
+        break;
+    }
+  }
+
+  toggleSonido() {
+    this.audioService.isActive = !this.audioService.isActive;
+    this.isAudioActive = this.audioService.isActive;
+  }
+}
